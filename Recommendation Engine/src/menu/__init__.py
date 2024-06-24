d253603645b70e17cd0b7665bbf4d06f3ae5a3bd
Rohan Pranav Gajeelee <rohan.p.gajeelee@intimetec.com>
2024-06-24 10:34:17 +0530

    Creation of new branch to hold refactored code and implementation of client server architecture

--- Recommendation Engine/src/menu/__init__.py
+++
@@ -1,4 +0,0 @@
-from .admin_menu import AdminMenu
-from .chef_menu import ChefMenu
-from .employee_menu import EmployeeMenu
-from .main_menu import MainMenu
--- Recommendation Engine/src/services/report_service.py
+++
@@ -1,9 +0,0 @@
-from src.models.reporting import Report
-
-class ReportService:
-    @staticmethod
-    def generate_report():
-        year = int(input("Enter year: "))
-        month = int(input("Enter month: "))
-        Report.generate_monthly_feedback_report(year, month)
-        return True
